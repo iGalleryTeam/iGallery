e109f47e6c2dfe04ff21bf818e480f5563f98914
Dariya Baibolatova <baibolatovads@gmail.com>
2020-04-15 03:10:41 +0600

    added model Managers, DRF Serializers, validators

--- back/api/models.py
+++ back/api/models.py
@@ -4,11 +4,68 @@ from django.db import models
 # Create your models here.
 
 
+class CreatedByUser(models.Manager):
+	def for_user(self, user):
+		return self.filter(created_by=user)
+
+
+class ModernArt(models.Manager):
+	def get_queryset(self):
+		return self.filter__gte(year_of_publishing=2000)
+
+
+class ClassicArt(models.Manager):
+	def get_queryset(self):
+		return self.filter__lte(year_of_publishing=2000)
+
+
+class Picture(models.Model):
+	name = models.CharField(max_length=255)
+	year_of_publishing = models.IntegerField()
+	likes = models.IntegerField(default=0)
+	image = models.ImageField(default=None)
+
+	objects = models.Manager()
+	created_by_user = CreatedByUser()
+	modern_art = ModernArt()
+	classic_art = ClassicArt()
+
+	class Meta:
+		verbose_name = 'Picture'
+		verbose_name_plural = 'Pictures'
+
+	def __str__(self):
+		return '{}: {}'.format(self.id, self.name)
+
+	def to_json(self):
+		return {
+			"id": self.id,
+			"name": self.name,
+			"year_of_publishing": self.year_of_publishing,
+			"likes": self.likes
+		}
+
+
+class VirtualGallery(models.Manager):
+	def get_queryset(self):
+		return self.filter(is_virtual=True)
+
+
+class NotVirtualGallery(models.Manager):
+	def get_queryset(self):
+		return self.filter(is_virtual=False)
+
+
 class Gallery(models.Model):
 	name = models.CharField(max_length=255)
 	address = models.CharField(max_length=255)
-	year = models.DateTimeField()
-	virtual = models.BooleanField(default=True)
+	date_of_opening = models.DateTimeField()
+	is_virtual = models.BooleanField(default=True)
+	picture = models.ForeignKey(Picture, on_delete=models.CASCADE)
+
+	objects = models.Manager()
+	virtual_gallery = VirtualGallery()
+	not_virtual_gallery = NotVirtualGallery()
 
 	class Meta:
 		verbose_name = 'Gallery'
@@ -22,31 +79,9 @@ class Gallery(models.Model):
 			"id": self.id,
 			"name": self.name,
 			"address": self.address,
-			"year": self.year,
-			"virtual": self.virtual
+			"date_of_opening": self.date_of_opening,
+			"picture": self.picture,
+			"is_virtual": self.is_virtual
 		}
 
-
-class Picture(models.Model):
-	name = models.CharField(max_length=255)
-	year = models.DateTimeField()
-	gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE, related_name='pictures')
-	likes = models.IntegerField(default=0)
-	image = models.ImageField(default=None)
-
-	class Meta:
-		verbose_name = 'Picture'
-		verbose_name_plural = 'Pictures'
-
-	def __str__(self):
-		return '{}: {}'.format(self.id, self.name)
-
-	def to_json(self):
-		return {
-			"id": self.id,
-			"name": self.name,
-			"year": self.year,
-			"gallery": self.gallery,
-			"likes": self.likes
-		}
 
--- back/api/serializers.py
+++ back/api/serializers.py
@@ -2,30 +2,6 @@ from rest_framework import serializers
 from models import Gallery, Picture
 
 
-class GallerySerializer(serializers.Serializer):
-	id = serializers.IntegerField(read_only=True)
-	name = serializers.CharField(required=True)
-
-	def create(self, validated_data):
-		gallery = Gallery(**validated_data)
-		gallery.save()
-		return gallery
-
-	def update(self, instance, validated_data):
-		instance.name = validated_data.get('name', instance.name)
-		instance.save()
-		return instance
-
-
-class GallerySerializer2(serializers.ModelSerializer):
-	id = serializers.IntegerField(read_only=True)
-	name = serializers.CharField(required=True)
-
-	class Meta:
-		model = Gallery
-		fields = '__all__'
-
-
 class PictureSerializer(serializers.Serializer):
 	id = serializers.IntegerField(read_only=True)
 	name = serializers.CharField(required=True)
@@ -41,10 +17,70 @@ class PictureSerializer(serializers.Serializer):
 		return instance
 
 
-class PictureSerializer2(serializers.ModelSerializer):
+class PictureModelSerializer(serializers.ModelSerializer):
 	id = serializers.IntegerField(read_only=True)
 	name = serializers.CharField(required=True)
 
 	class Meta:
 		model = Picture
 		fields = '__all__'
+
+	def validate_name(self, value):
+		if['/', '^', '$', '%', '`'] in value:
+			raise serializers.ValidationError('invalid character in name field')
+		return value
+
+	def validate_date_of_publishing(self, value):
+		if value < 0 or value > 2020:
+			raise serializers.ValidationError('invalid date_of_publishing of the picture')
+		return value
+
+	def validate_likes(self, value):
+		if value < 0:
+			raise serializers.ValidationError('invalid number of likes')
+		return value
+
+
+class GallerySerializer(serializers.Serializer):
+	id = serializers.IntegerField(read_only=True)
+	name = serializers.CharField(required=True)
+
+	def create(self, validated_data):
+		gallery = Gallery(**validated_data)
+		gallery.save()
+		return gallery
+
+	def update(self, instance, validated_data):
+		instance.name = validated_data.get('name', instance.name)
+		instance.save()
+		return instance
+
+
+class GalleryShortSerializer(serializers.ModelSerializer):
+	id = serializers.IntegerField(read_only=True)
+	name = serializers.CharField(required=True)
+	picture_id = serializers.IntegerField(write_only=True)
+
+	class Meta:
+		model = Gallery
+		fields = ('id', 'name', 'address', 'date_of_opening', 'is_virtual', 'picture_id')
+
+	def validate_name(self, value):
+		if['/', '^', '$', '%'] in value:
+			raise serializers.ValidationError('invalid character in name field')
+		return value
+
+	def validate_date_of_opening(self, value):
+		if value < 0:
+			raise serializers.ValidationError('invalid date type')
+		return value
+
+
+
+class GalleryFullSerializer(GalleryShortSerializer):
+	picture = PictureSerializer(read_only=True)
+
+	class Meta(GalleryShortSerializer.Meta):
+		fields = GalleryShortSerializer.Meta.fields + ('picture', )
+
+
